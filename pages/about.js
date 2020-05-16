c6fe09e8ba67888b25313b4c2a01a03daa4fb8ed
Timothy D Ngo <37946614+NgoTimothy@users.noreply.github.com>
2020-05-16 14:59:20 -0700

    Working environment setup for nextJS

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,7 @@
-import Layout from '../components/MyLayout';
-
-export default function About() {
-    return (
-      <div>
-        <Layout>
-          <p>This is the About Page</p>
-      </Layout>
-      </div>
-    );
-}
-
+import Link from 'next/link'
+export default () => (
+  <div>
+    <div>About us</div>
+    <div>Back to <Link href='/' as={ process.env.BACKEND_URL + '/'}><a>Home</a></Link></div>
+  </div>
+)
