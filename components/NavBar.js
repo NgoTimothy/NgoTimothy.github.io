61870c4e26ab6ce876b2b9a23c1d12e5c9b21391
Timothy D Ngo <37946614+NgoTimothy@users.noreply.github.com>
2020-05-21 23:47:54 -0700

    coloring links

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,24 +1,36 @@
 import React from 'react'
 import Link from 'next/link'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown, Dropdown } from 'react-bootstrap'
+
+const styles = {
+  links: {
+    fontColor: 'white'
+  }
+}
 
 export default () => (
   <div>
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href='.'>LOL</Navbar.Brand>
+    <Navbar bg='dark' bg='dark' variant='dark' expand='md'>
+      <Navbar.Brand href='.'><img className='rounded-circle w-50 p-3 h-25 d-none d-md-block text-center' alt='100x100' src='/static/rsz_headshot.jpg/'/></Navbar.Brand>
       <Navbar.Toggle aria-controls='responsive-navbar-nav' />
       <Navbar.Collapse id='responsive-navbar-nav'>
-        <Nav className="ml-auto">
-          <Nav.Link className="mx-auto" href="#">Hi</Nav.Link>
-          <NavDropdown title="News" className="mx-auto" id='collasible-nav-dropdown'>
-          <NavDropdown.Item>Business</NavDropdown.Item>
-          <NavDropdown.Divider/>
-          <NavDropdown.Item>Technology</NavDropdown.Item>
+        <Nav className='ml-auto'>
+          <Nav.Link className='mx-auto d-md-block pr-lg-4 text-white' href='/'>Home</Nav.Link>
+          <Nav.Link className='mx-auto d-md-block pr-lg-4 text-white' href='pdfs/Resume_Timothy_Ngo_Spring_2020.pdf/' target='_blank'>Resume</Nav.Link>
+          <NavDropdown title={<span className='text-white'>Projects</span>} className='mx-auto d-md-block pr-lg-3' id='collasible-nav-dropdown'>
+            <NavDropdown.Item>Crime and Dime</NavDropdown.Item>
+            <NavDropdown.Divider/>
+            <NavDropdown.Item>Learnhub</NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link className="mx-auto" href="#">About</Nav.Link>
+          <Nav.Link className='mx-auto d-md-block pr-lg-5 text-white' href='/about'>About</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   </div>
 )
 {/* <Link href='/about' as={ process.env.BACKEND_URL + '/about'}><h1>About</h1> </Link>  */}
+{/* <NavDropdown title='News' className='mx-auto' id='collasible-nav-dropdown'>
+          <NavDropdown.Item>Business</NavDropdown.Item>
+          <NavDropdown.Divider/>
+          <NavDropdown.Item>Technology</NavDropdown.Item>
+          </NavDropdown> */}
