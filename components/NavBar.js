ae8d0c80c620041a20304edaee5d56f98d7e1d09
Timothy D Ngo <37946614+NgoTimothy@users.noreply.github.com>
2020-05-23 13:42:20 -0700

    Add project links

--- components/NavBar.js
+++ components/NavBar.js
@@ -18,9 +18,11 @@ export default () => (
           <Nav.Link className='mx-auto d-md-block pr-lg-4 pr-3 text-white' href='/'>Home</Nav.Link>
           <Nav.Link className='mx-auto d-md-block pr-lg-4 pr-3 text-white' href='pdfs/Resume_Timothy_Ngo_Spring_2020.pdf/' target='_blank'>Resume</Nav.Link>
           <NavDropdown title={<span className='text-white'>Projects</span>} className='mx-auto d-md-block pr-lg-3 pr-0' id='collasible-nav-dropdown'>
-            <NavDropdown.Item>Crime and Dime</NavDropdown.Item>
+            <NavDropdown.Item href='https://github.com/NgoTimothy/CrimeAndDime/' target='_blank' passHref={true}>Crime and Dime</NavDropdown.Item>
             <NavDropdown.Divider/>
-            <NavDropdown.Item>Learnhub</NavDropdown.Item>
+            <NavDropdown.Item href='https://github.com/NgoTimothy/LearnHub/' target='_blank'>Learnhub</NavDropdown.Item>
+            <NavDropdown.Divider/>
+            <NavDropdown.Item href='https://github.com/NgoTimothy/Custom-Alphabet-Word-Sorter/' target='_blank'>Customer Word Sorter</NavDropdown.Item>
           </NavDropdown>
           <Nav.Link className='mx-auto d-md-block pr-lg-5 pr-3 text-white' href='/about'>About</Nav.Link>
         </Nav>
